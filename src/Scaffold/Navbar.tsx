571b68a7cf596335057ec6a7c24356642b63d57a
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-04-09 11:42:37 +0530

    refactor(navbar): segregate navbar data from presentation logic

--- src/Scaffold/Navbar.tsx
+++ src/Scaffold/Navbar.tsx
@@ -1,6 +1,16 @@
 import { Navbar as MantineNavbar, Text, useMantineTheme } from '@mantine/core';
 import { useThemedValue } from '../hooks';
 
+type NavElement = {
+  path: string;
+  title: string;
+};
+const navData: NavElement[] = [
+  { path: '/', title: 'Home' },
+  { path: 'welearn', title: 'WeLearn' },
+  { path: 'student-mess', title: 'Mess' },
+];
+
 function Navbar({ isOpen }: { isOpen: boolean }) {
   const theme = useMantineTheme();
   return (
@@ -10,15 +20,11 @@ function Navbar({ isOpen }: { isOpen: boolean }) {
       hidden={!isOpen}
       width={{ sm: 120 }}
     >
-      <MantineNavbar.Section p="xs">
-        <Text color={useThemedValue(theme.white, theme.black)}>Home</Text>
-      </MantineNavbar.Section>
-      <MantineNavbar.Section p="xs">
-        <Text color={useThemedValue(theme.white, theme.black)}>WeLearn</Text>
-      </MantineNavbar.Section>
-      <MantineNavbar.Section p="xs">
-        <Text color={useThemedValue(theme.white, theme.black)}>Mess</Text>
-      </MantineNavbar.Section>
+      {navData.map(({ path, title }) => (
+        <MantineNavbar.Section p="xs" key={title}>
+          <Text color={useThemedValue(theme.white, theme.black)}>{title}</Text>
+        </MantineNavbar.Section>
+      ))}
     </MantineNavbar>
   );
 }
