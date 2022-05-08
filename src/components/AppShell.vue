307d02f88dc2bc65f178b235b93ff140cb7b677b
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-05-08 17:33:23 +0530

    feat(src): add App Shell
    The app shell is the wrapper which will be present in all pages

---
+++ src/components/AppShell.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import MenuBars from '../components/icons/MenuBars.vue';
+</script>
+
+<template>
+  <div class="navbar bg-base-300 px-4">
+    <div class="flex-none">
+      <!-- Navigation Panel Toggler -->
+      <button class="btn btn-ghost btn-square btn-sm lg:hidden">
+        <label for="drawer-handle">
+          <MenuBars />
+        </label>
+      </button>
+    </div>
+    <div class="flex-1">
+      <slot name="header-main">
+        <!-- Header Main Section -->
+      </slot>
+    </div>
+    <div class="flex-none lg:px-4">
+      <slot name="header-right">
+        <!-- Header Right Section -->
+      </slot>
+    </div>
+  </div>
+  <div class="drawer-mobile drawer">
+    <input id="drawer-handle" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content flex flex-col items-center">
+      <slot name="page-content">
+        <!-- Page content here -->
+      </slot>
+    </div>
+    <div class="drawer-side">
+      <label for="drawer-handle" class="drawer-overlay"></label>
+      <slot name="nav-panel">
+        <!-- Navigation Panel Content -->
+      </slot>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppShell from '../components/AppShell.vue';
+import MenuDots from '../components/icons/MenuDots.vue';
+</script>
 
 <template>
-  <main>Welcome To IISER K</main>
+  <main data-theme="night" class="h-screen overflow-y-hidden">
+    <AppShell>
+      <template v-slot:header-main>
+        <a class="px-4 text-2xl normal-case">IISER K App</a>
+      </template>
+      <template v-slot:header-right>
+        <button class="btn btn-ghost btn-circle btn-sm">
+          <MenuDots />
+        </button>
+      </template>
+      <template v-slot:page-content>Welcome to IISER K App</template>
+      <template v-slot:nav-panel>
+        <ul class="menu w-max bg-base-300 p-4 text-lg">
+          <li><a>Home</a></li>
+          <li><a>WeLearn</a></li>
+          <li><a>Mess</a></li>
+        </ul>
+      </template>
+    </AppShell>
+  </main>
 </template>
