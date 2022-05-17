0645e80c6a28634f41cb1b51f48c546a72e454ef
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-05-17 22:32:09 +0530

    feat(welearn): add Welearn Login page

---
+++ src/components/WelearnLogin.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/auth';
+import { storeToRefs } from 'pinia';
+
+defineEmits(['login']);
+
+const { username, password } = storeToRefs(useAuthStore());
+</script>
+<template>
+  <span class="text-2xl">Login to WeLearn</span>
+  <form
+    class="form-control w-2/3 lg:w-1/2"
+    @submit.prevent="$emit('login', username, password)"
+  >
+    <label class="label">
+      <span class="label-text text-lg">Username</span>
+    </label>
+    <input
+      v-model.lazy="username"
+      type="text"
+      placeholder="Type here"
+      class="input input-bordered w-full"
+    />
+    <label class="label">
+      <span class="label-text text-lg">Password</span>
+    </label>
+    <input
+      v-model="password"
+      type="password"
+      placeholder="Type here"
+      class="input input-bordered w-full"
+    />
+    <button
+      type="submit"
+      class="btn btn-primary my-6"
+      @click.prevent="$emit('login', username, password)"
+    >
+      Login
+    </button>
+  </form>
+</template>
--- src/views/WeLearnView.vue
+++ src/views/WeLearnView.vue
@@ -1,1 +1,20 @@
-<template>WeLearn</template>
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+
+import { WeLearnClient } from '@/models/welearn/client';
+import { useAuthStore } from '@/stores/auth';
+
+import WelearnLogin from '@/components/WelearnLogin.vue';
+
+const { isLoggedIn } = storeToRefs(useAuthStore());
+const login = async (username: string, password: string) => {
+  isLoggedIn.value = await WeLearnClient.login(username, password);
+};
+</script>
+
+<template>
+  <template v-if="isLoggedIn"> </template>
+  <template v-else>
+    <WelearnLogin @login="login" />
+  </template>
+</template>
