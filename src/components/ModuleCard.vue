99c5980c8aa08c96e332ea8d424ffcb58bcdd47e
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-05-22 10:38:02 +0530

    feat(welearn): make links and resources(pdf, etc) modules functional

--- src/components/ModuleCard.vue
+++ src/components/ModuleCard.vue
@@ -2,26 +2,45 @@
 const props = defineProps<{
   courseModule: any;
 }>();
+// const clickHandler = (modname: string, url: string) => {
+const clickHandler = (courseModule: any) => {
+  switch (courseModule.modname) {
+    case 'url':
+    case 'resource':
+      window.open(courseModule.url);
+      break;
+    default:
+      break;
+  }
+};
 </script>
 <template>
   <div
     class="cursor-pointer rounded-md border border-neutral-focus p-2 hover:border-secondary"
+    @click="clickHandler(props.courseModule)"
   >
-    <div class="flex items-center gap-2">
+    <div
+      class="mb-2 flex items-center gap-2"
+      v-if="props.courseModule.modname !== 'label'"
+    >
       <img
         class="h-8 w-8 flex-shrink-0 rounded-full"
         :src="props.courseModule.modicon"
       />
-      <div class="no-overflow cursor-pointer text-lg">
+      <div class="cursor-pointer truncate text-lg">
         {{ props.courseModule.name }}
       </div>
     </div>
-    <div v-html="props.courseModule.description"></div>
+    <div
+      :data-isCompact="props.courseModule.isCompact"
+      v-html="props.courseModule.description"
+    ></div>
   </div>
 </template>
 
 <style>
-.no-overflow {
+div[data-isCompact='false'] .no-overflow {
+  /* Same as: '@apply truncate' */
   display: -webkit-box;
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
