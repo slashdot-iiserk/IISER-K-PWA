a12cac863ba739ff49ac2646850c209a11a22f3f
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2022-06-03 16:19:13 +0530

    refactor(utils): move time constants into utils folder

--- src/hooks/cachedFetch.ts
+++ src/hooks/cachedFetch.ts
@@ -1,18 +1,14 @@
+import { TIME } from '@/utils/datetime';
 import useSWRV from 'swrv';
 import LocalStorageCache from 'swrv/dist/cache/adapters/localStorage';
 import type { fetcherFn, IKey } from 'swrv/dist/types';
-
-const second = 1000;
-const minute = 60 * 1000;
-const hour = 60 * minute;
-export const TIME = { second, minute, hour };
 
 const localStorageCache = new LocalStorageCache();
 
 const useCachedFetch = <T>(
   key: IKey,
   fetcher: fetcherFn<T>,
-  dedupingInterval = 1 * hour,
+  dedupingInterval = 1 * TIME.hour,
 ) => {
   const { data, error, isValidating, mutate } = useSWRV<T, any>(key, fetcher, {
     cache: localStorageCache,
---
+++ src/utils/datetime.ts
@@ -0,0 +1,6 @@
+const milliSecond = 1;
+const second = 1000 * milliSecond;
+const minute = 60 * second;
+const hour = 60 * minute;
+const day = 24 * hour;
+export const TIME = { second, minute, hour, day, milliSecond };
